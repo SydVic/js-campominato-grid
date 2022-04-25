94297863ec8c6dd38e1d8626b3d559957183569d
SydVic <fabrizio.bocca27@gmail.com>
2022-04-25 10:10:30 +0200

    risolto problema undefined al posto dei numeri nelle celle

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 // devo creare una griglia di dimensioni 10 x 10 (posso già pensarla in base al fatto che poi dovra essere 9 x 9 e 7 x 7?)
 // inizio creando un ciclo for per stampare i numeri da 1 a 100
+
 let boxQuantity = 100;
 
-let gridBoxNumber = [];
+
 const gridContainer = document.querySelector(".grid-container");
 for ( let i = 1; i <= boxQuantity; i++ ) {
 
-    const boxNumber = boxQuantity[i];
-
+    // assegno il numero al box
+    const boxNumber = i;
+    console.log(boxNumber);
+    
+    // creo l'elemento in html con il numero al suo interno
     const gridBox = document.createElement("div");
     gridBox.innerHTML = `<span>${boxNumber}</span>`;
 
+    // aggiungo la classe css
     gridBox.classList.add("grid-box");
 
+    // gestione click elemento
+    gridBox.addEventListener("click", function() {
+        this.classList.add("clicked");
+    })
+
+    // inserisco gli elementi nel container
     gridContainer.append(gridBox);
 }
-console.log(gridBoxNumber);
