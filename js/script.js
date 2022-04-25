5680f2b5bde1552af2eedab90f080366581b65f7
SydVic <fabrizio.bocca27@gmail.com>
2022-04-25 10:50:25 +0200

    aggiunto selettore difficoltà

--- js/script.js
+++ js/script.js
@@ -1,8 +1,43 @@
-// devo creare una griglia di dimensioni 10 x 10 (posso già pensarla in base al fatto che poi dovra essere 9 x 9 e 7 x 7?)
-// inizio creando un ciclo for per stampare i numeri da 1 a 100
+const playBtn = document.getElementById("play-game");
 
+playBtn.addEventListener("click", function() {
+
+    const userDifficulty = document.getElementById("user-difficulty").value;
+    console.log(userDifficulty);
+
+
+    let boxQuantity = 100;
+
+    const gridContainer = document.querySelector(".grid-container");
+    for ( let i = 1; i <= boxQuantity; i++ ) {
+    
+        // assegno il numero al box
+        const boxNumber = i;
+        console.log(boxNumber);
+        
+        // creo l'elemento in html con il numero al suo interno
+        const gridBox = document.createElement("div");
+        gridBox.innerHTML = `<span>${boxNumber}</span>`;
+    
+        // aggiungo la classe css
+        gridBox.classList.add("grid-box");
+    
+        // gestione click elemento
+        gridBox.addEventListener("click", function() {
+            this.classList.add("clicked");
+        })
+    
+        // inserisco gli elementi nel container
+        gridContainer.append(gridBox);
+    }
+
+
+
+})
+
+
+/*
 let boxQuantity = 100;
-
 
 const gridContainer = document.querySelector(".grid-container");
 for ( let i = 1; i <= boxQuantity; i++ ) {
@@ -26,3 +61,4 @@ for ( let i = 1; i <= boxQuantity; i++ ) {
     // inserisco gli elementi nel container
     gridContainer.append(gridBox);
 }
+*/
