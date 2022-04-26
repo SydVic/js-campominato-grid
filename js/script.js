7fbb58204ef72ee6572047574dd09f032c6aa049
SydVic <fabrizio.bocca27@gmail.com>
2022-04-26 09:22:51 +0200

    ripulita parte commentata

--- js/script.js
+++ js/script.js
@@ -5,10 +5,22 @@ playBtn.addEventListener("click", function() {
     const userDifficulty = document.getElementById("user-difficulty").value;
     console.log(userDifficulty);
 
+    let boxQuantity = 0;
+    let boxSize = "";
 
-    let boxQuantity = 100;
+    if (userDifficulty === "easy") {
+        boxQuantity = 100;
+        boxSize = "easy";
+    } else if (userDifficulty === "hard") {
+        boxQuantity = 81;
+        boxSize = "hard";
+    } else if (userDifficulty === "crazy") {
+        boxQuantity = 49;
+        boxSize = "crazy";
+    }
 
     const gridContainer = document.querySelector(".grid-container");
+    gridContainer.innerHTML = "";
     for ( let i = 1; i <= boxQuantity; i++ ) {
     
         // assegno il numero al box
@@ -21,6 +33,7 @@ playBtn.addEventListener("click", function() {
     
         // aggiungo la classe css
         gridBox.classList.add("grid-box");
+        gridBox.classList.add(boxSize);
     
         // gestione click elemento
         gridBox.addEventListener("click", function() {
@@ -30,35 +43,4 @@ playBtn.addEventListener("click", function() {
         // inserisco gli elementi nel container
         gridContainer.append(gridBox);
     }
-
-
-
 })
-
-
-/*
-let boxQuantity = 100;
-
-const gridContainer = document.querySelector(".grid-container");
-for ( let i = 1; i <= boxQuantity; i++ ) {
-
-    // assegno il numero al box
-    const boxNumber = i;
-    console.log(boxNumber);
-    
-    // creo l'elemento in html con il numero al suo interno
-    const gridBox = document.createElement("div");
-    gridBox.innerHTML = `<span>${boxNumber}</span>`;
-
-    // aggiungo la classe css
-    gridBox.classList.add("grid-box");
-
-    // gestione click elemento
-    gridBox.addEventListener("click", function() {
-        this.classList.add("clicked");
-    })
-
-    // inserisco gli elementi nel container
-    gridContainer.append(gridBox);
-}
-*/
